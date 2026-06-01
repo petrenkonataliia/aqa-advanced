0da17666707b2a598a01876c3933c8e7e193c585
natalie <nataliiapetrenko6@gmail.com>
2026-06-01 15:15:36 +0200

    Add homework 6.1 tasks

---
+++ homework_6.1./task3.js
@@ -0,0 +1,13 @@
+function checkOrder(available, ordered) {
+    if (ordered === 0) {
+        return "Your order is empty";
+    } else if (ordered > available) {
+        return "Your order is too large, we don’t have enough goods.";
+    } else {
+        return "Your order is accepted";
+    }
+}
+
+console.log(checkOrder(100, 50));
+console.log(checkOrder(100, 150));
+console.log(checkOrder(100, 0));
