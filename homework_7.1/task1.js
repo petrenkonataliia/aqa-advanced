a32cdfd67429b13621f4b4a9bef52ea49facac1a
natalie <nataliiapetrenko6@gmail.com>
2026-06-03 21:11:29 +0200

    Add homework 7.1

---
+++ homework_7.1/task1.js
@@ -0,0 +1,18 @@
+function handleEven() {
+    console.log("number is even");
+}
+
+function handleOdd() {
+    console.log("number is odd");
+}
+
+function handleNum(number, evenCallback, oddCallback) {
+    if (number % 2 === 0) {
+        evenCallback();
+    } else {
+        oddCallback();
+    }
+}
+
+handleNum(10, handleEven, handleOdd);
+handleNum(7, handleEven, handleOdd);
